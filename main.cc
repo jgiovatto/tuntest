b900afc1b73a71dcceecdf0f655d98a8e8fb63f1
Joseph Giovatto <jgiovatto@adjacentlink.com>
2021-02-07 16:18:29 -0500

    added udp msg builder

--- main.cc
+++ main.cc
@@ -1,8 +1,9 @@
 // jgiovatto@adjacentlink.com
 // Jan 29, 2021
 // tuntest tool to play with ip/arp frames
-
-// try arping 172.16.1.99 and ping 172.16.1.99
+//
+// may need to pop open the new interface
+// sudo iptables -I INPUT -i tuntap0 -j ACCEPT
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -33,13 +34,19 @@
 #define COLOR_RED "\033[0;31m"
 #define COLOR_YEL "\033[1;33m"
 
-#define PROTO_ETH COLOR_BLU
 
 #include <vector>
 
+// local/remote ip and hardware interface addrs
+const char * localIP  = "172.16.0.1";
+const char * localHW  = "02:02:00:00:00:01";
+
+const char * remoteIP = "172.16.0.99";
+const char * remoteHW = "02:02:00:00:00:63";
+
+// add some color to our logs
 struct Color {
   Color(const char * c) : c_(c) { }
-
   const char * c_;
 };
 
@@ -48,6 +55,7 @@ using Colors = std::vector<Color>;
 
 bool bRunning = true;
 
+
 void bye(const char *msg)
 {
     perror(msg);
@@ -55,9 +63,9 @@ void bye(const char *msg)
 }
 
 
-void print_hex(const char * buff, size_t bufflen, const Colors & colors)
+void print_hex(const char * buff, size_t const bufflen, const Colors & colors)
 {
-    const  size_t lineLen = 16;
+    const size_t lineLen = 16;
 
     size_t linenum = 0;
     size_t pos = 0;
@@ -68,60 +76,58 @@ void print_hex(const char * buff, size_t bufflen, const Colors & colors)
           size_t pos;
           char   buf[128];
           Str() : pos(0), buf{0} { } 
-        }s1, s2;
+        }strHex, strTxt;
 
         for(size_t idx = 0; idx < lineLen; ++idx, ++pos)
         {
             if((idx != 0) && (idx % (lineLen / 2) == 0))
             {
-                s1.pos += snprintf(s1.buf + s1.pos, sizeof(s1.buf) - s1.pos, " ");
-                s2.pos += snprintf(s2.buf + s2.pos, sizeof(s2.buf) - s2.pos, " ");
+                strHex.pos += snprintf(strHex.buf + strHex.pos, sizeof(strHex.buf) - strHex.pos, " ");
+                strTxt.pos += snprintf(strTxt.buf + strTxt.pos, sizeof(strTxt.buf) - strTxt.pos, " ");
             }
 
-            // have value
             if(pos < bufflen)
             {
                 const char val = buff[pos];
 
                 // as hex
-                s1.pos += snprintf(s1.buf + s1.pos, sizeof(s1.buf) - s1.pos, "%s%02hhx ", colors[pos].c_, (uint8_t) val);
+                strHex.pos += snprintf(strHex.buf + strHex.pos, sizeof(strHex.buf) - strHex.pos, "%s%02hhx ", colors[pos].c_, (uint8_t) val);
 
                 // as printable
-                s2.pos += snprintf(s2.buf + s2.pos, sizeof(s2.buf) - s2.pos, "%c", isprint(val) ? val : '.');
+                strTxt.pos += snprintf(strTxt.buf + strTxt.pos, sizeof(strTxt.buf) - strTxt.pos, "%c", isalnum(val) ? val : '.');
             }
             else
             {
                 // fill the last line
-                s2.pos += snprintf(s2.buf + s2.pos, sizeof(s2.buf) - s2.pos, " ");
+                strHex.pos += snprintf(strHex.buf + strHex.pos, sizeof(strHex.buf) - strHex.pos, "   ");
+                strTxt.pos += snprintf(strTxt.buf + strTxt.pos, sizeof(strTxt.buf) - strTxt.pos, " ");
             }
         }
-        printf("%03zu  %s    %s\n", linenum * lineLen, s2.buf, s1.buf);
+        printf("%03zu  %s    %s\n", linenum * lineLen, strTxt.buf, strHex.buf);
 
         ++linenum;
     }
 
-   printf("\n");
+   printf("%s\n", COLOR_NRM);
 }
 
-uint16_t csum16(const void *buff, uint16_t len)
+
+uint16_t csum16(const void *buff, uint16_t len, uint16_t carry = 0)
 {
-  uint32_t sum = 0;
+  uint32_t sum = carry;
 
   uint16_t const * p = (uint16_t const *) buff;
 
-  while(len > 1)
-   {
+  while(len > 1) {
      sum += *p++;
      len -= 2;
    }
 
-  if(len > 0)
-   {
+  if(len > 0) {
      sum += *((uint8_t *) p);
    }
 
-  while(sum >> 16)
-   {
+  while(sum >> 16) {
      sum = (sum & 0xFFFF) + (sum >> 16);
    }
 
@@ -129,20 +135,27 @@ uint16_t csum16(const void *buff, uint16_t len)
 }
 
 
-
-int bounce_frame(char *buff, size_t len)
+uint16_t csum16v(const struct iovec * iov, const size_t iovn)
 {
-    int result = 0;
+  uint16_t sum = 0;
 
+  for(size_t n = 0; n < iovn; ++n)
+   {
+     sum = csum16(iov[n].iov_base, iov[n].iov_len, sum);
+   }
+
+  return sum;
+}
+
+
+int parse_frame(char *buff, size_t len)
+{
     Colors colors(len, Color(COLOR_NRM));
+
+    int result = 0;
 
     if(len > sizeof(ether_header))
     {
-        // make up nbr
-        ether_addr faux_nbr;
-
-        memcpy(&faux_nbr, ether_aton("02:02:00:00:00:99"), ETH_ALEN);
-
         struct ether_header * eth = (struct ether_header *) buff;
 
         const uint16_t ether_type = htons(eth->ether_type);
@@ -150,13 +163,11 @@ int bounce_frame(char *buff, size_t len)
         colors[12].c_ = COLOR_YEL;
         colors[13].c_ = COLOR_YEL;
 
-        // sanity check for eth arp
+        // sanity check for eth arp eth/ipv4
         if((len == sizeof(struct ether_header) + sizeof(struct arphdr) + (2 * (ETH_ALEN + 4))) && 
            (ether_type == ETHERTYPE_ARP))
         {
-            size_t offset = sizeof(ether_header);
-
-            struct arphdr * arp = (struct arphdr*) (buff + offset);
+            struct arphdr * arp = (struct arphdr*) (eth + 1);
        
             if((ntohs(arp->ar_op)  == ARPOP_REQUEST) &&
                (ntohs(arp->ar_hrd) == ARPHRD_ETHER)  &&
@@ -164,20 +175,7 @@ int bounce_frame(char *buff, size_t len)
                (arp->ar_hln        == ETH_ALEN)      &&
                (arp->ar_pln        == 4))
             {
-                // eth hdr [[ff ff ff ff ff ff]
-                //          [ee 66 e3 bc 9c b7]
-                //          [08 06]]
-                //
-                // arp hdr [[00 01]
-                //          [08 00]
-                //          [06]
-                //          [04]
-                //          [00 01]
-                //          [02 02 00 00 00 01]  [ac 10 01 01]
-                //          [00 00 00 00 00 00]  [ac 10 01 02]]
-
-                struct arp_ipv4_req_
-                {
+                struct arp_ipv4_req_ {
                     struct ether_addr src_hw;
                     in_addr           src_ip;
 
@@ -185,12 +183,15 @@ int bounce_frame(char *buff, size_t len)
                     in_addr           tar_ip;
                 } __attribute__((packed)) * arp_req = (struct arp_ipv4_req_ *) (arp + 1);
 
+#ifdef DEBUG
                 printf(COLOR_YEL "in  ETH ARP:");
-                printf("t_hw [%s]", ether_ntoa(&arp_req->tar_hw));
+                printf("t_hw [%s]",   ether_ntoa(&arp_req->tar_hw));
                 printf(", s_hw [%s]", ether_ntoa(&arp_req->src_hw));
+
                 printf(", s_ip [%s]", inet_ntoa(arp_req->src_ip));
                 printf(", t_ip [%s]", inet_ntoa(arp_req->tar_ip));
                 printf("\n" COLOR_NRM);
+#endif
 
                 // swap src/target ip addrs
                 const in_addr tmp_ip = arp_req->tar_ip;
@@ -198,30 +199,32 @@ int bounce_frame(char *buff, size_t len)
                 arp_req->src_ip = tmp_ip;
 
                 // swap src/target hw addrs
-                memcpy(&arp_req->tar_hw, &arp_req->src_hw, ETH_ALEN);
-                memcpy(&arp_req->src_hw, &faux_nbr, ETH_ALEN);
+                memcpy(&arp_req->tar_hw, &arp_req->src_hw,     ETH_ALEN);
+                memcpy(&arp_req->src_hw, ether_aton(remoteHW), ETH_ALEN);
             
                 // set as reply   
                 arp->ar_op = htons(ARPOP_REPLY);
 
+#ifdef DEBUG
                 printf(COLOR_YEL "out ETH ARP:");
-                printf("t_hw [%s]", ether_ntoa(&arp_req->tar_hw));
+                printf("t_hw [%s]",   ether_ntoa(&arp_req->tar_hw));
                 printf(", s_hw [%s]", ether_ntoa(&arp_req->src_hw));
+
                 printf(", s_ip [%s]", inet_ntoa(arp_req->src_ip));
                 printf(", t_ip [%s]", inet_ntoa(arp_req->tar_ip));
                 printf("\n" COLOR_NRM);
+#endif
 
                 // return complete arp reply
                 result = len;
             }
         }
-        // sanity check for eth ipv4
+        // sanity check for eth/ipv4
         else if((len > sizeof(struct iphdr)) && (ether_type == ETHERTYPE_IP))
         {
-            size_t offset = sizeof(ether_header);
+            struct iphdr * ip = (struct iphdr*) (eth + 1);
 
-            struct iphdr * ip = (struct iphdr*) (buff + offset);
-
+            // set ip colors
             colors[14].c_ = COLOR_GRN;
             colors[23].c_ = COLOR_GRN;
  
@@ -231,9 +234,7 @@ int bounce_frame(char *buff, size_t len)
                  {
                     case IPPROTO_ICMP:
                      {
-                       offset += (ip->ihl << 2);
-
-                       struct icmphdr * icmp = (struct icmphdr *)(buff + offset);
+                       struct icmphdr * icmp = (struct icmphdr *)(buff + (ip->ihl << 2) + sizeof(ether_header));
 
                        const size_t icmplen = ntohs(ip->tot_len) - (ip->ihl << 2);
 
@@ -241,10 +242,18 @@ int bounce_frame(char *buff, size_t len)
                         {
                            if(icmp->type == ICMP_ECHO)
                              {
+                               struct {
+                                 uint32_t otime;
+                                 uint32_t rtime;
+                                 uint32_t ttime;
+                                } id_ts; // XXX TODO check/set timestamps
+
                                printf("ETH IPv4 ICMP ECHO_REQ seq %hu\n", ntohs(icmp->un.echo.sequence));
 
                                // turn into echo reply
                                icmp->type = ICMP_ECHOREPLY;
+
+                               // icmp checksum
                                icmp->checksum = 0;
                                icmp->checksum = ~csum16(icmp, icmplen);
 
@@ -252,20 +261,11 @@ int bounce_frame(char *buff, size_t len)
                                const in_addr_t tmp_ip = ip->saddr;
                                ip->saddr = ip->daddr;
                                ip->daddr = tmp_ip;
-
-
-                               struct
-                                {
-                                 uint32_t otime;
-                                 uint32_t rtime;
-                                 uint32_t ttime;
-                                } id_ts;
-
-
+ 
+                               // ip checksum
                                ip->check = 0;
                                ip->check = ~csum16(ip, ip->ihl << 2);
 
-                               // XXX TODO check len and timestamps
                                result = len;
                             }
                         }
@@ -284,20 +284,124 @@ int bounce_frame(char *buff, size_t len)
              }
         }
 
-
       // something to send back
       if(result > 0)
        {
-         // must swap eth src/dst
-         memcpy(&eth->ether_dhost, &eth->ether_shost, ETH_ALEN);
-         memcpy(&eth->ether_shost, &faux_nbr, ETH_ALEN);
+         // swap eth src/dst
+         memcpy(&eth->ether_dhost, &eth->ether_shost,    ETH_ALEN);
+         memcpy(&eth->ether_shost, ether_aton(remoteHW), ETH_ALEN);
        }
 
        print_hex(buff, len, colors);
     }
 
-
    return result;
+}
+
+
+
+int build_udp(char *buff, size_t bufflen)
+{
+   Colors colors(bufflen, Color(COLOR_NRM));
+
+   memset(buff, 0x0, bufflen);
+ 
+   struct ether_header eth;
+
+   // set eth src/dst
+   memcpy(&eth.ether_dhost, ether_aton(localHW),  ETH_ALEN); // local
+   memcpy(&eth.ether_shost, ether_aton(remoteHW), ETH_ALEN); // remote
+
+   eth.ether_type = htons(ETHERTYPE_IP);
+
+   // eth proto colors
+   colors[12].c_ = COLOR_YEL;
+   colors[13].c_ = COLOR_YEL;
+
+   // udp payload 
+   const char * msg = "hello";
+
+   // udp header and data len
+   const uint16_t udplen = 8 + strlen(msg);
+
+   // udp header
+   struct {
+      uint16_t sport;
+      uint16_t dport;
+      uint16_t len;
+      uint16_t sum;
+    } __attribute__((packed)) udp = {htons(0x1000), 
+                                     htons(0x1000), 
+                                     htons(udplen),
+                                     0};
+
+   // udp payload colors
+   for(size_t n = 0; n < strlen(msg); ++n)
+     {
+       colors[42 + n] = COLOR_CYN;
+     }
+ 
+   // udp pseudo sum
+   struct {
+     uint32_t src, dst;
+     uint8_t  res, proto;
+     uint16_t len;
+   } __attribute__((packed)) psum = {inet_addr(remoteIP), // remote
+                                     inet_addr(localIP),  // local
+                                     0,
+                                     IPPROTO_UDP,
+                                     htons(udplen)};
+
+   
+   struct iphdr ip;
+   ip.version  = 4;
+   ip.ihl      = 5;
+   ip.tos      = 0;
+   ip.tot_len  = htons((ip.ihl << 2) + udplen);
+   ip.id       = htons(0);
+   ip.frag_off = htons(0);
+   ip.ttl      = 1;
+   psum.proto  = ip.protocol = IPPROTO_UDP;
+   ip.check    = htons(0);
+   psum.src    = ip.saddr = inet_addr(remoteIP);
+   psum.dst    = ip.daddr = inet_addr(localIP);
+
+
+   const struct iovec iov[3] = {{(void*)&psum, sizeof(psum)}, 
+                                {(void*)&udp,  sizeof(udp)},
+                                {(void*)msg,   strlen(msg)}};
+
+   // udp csum
+   udp.sum = ~csum16v(iov, 3);
+
+   // ip csum
+   ip.check = ~csum16(&ip, ip.ihl << 2);
+
+   colors[14].c_ = COLOR_GRN;
+   colors[23].c_ = COLOR_GRN;
+
+   // load the segments, could use iovec here but print_hex isnt that good yet.
+   int offset = 0;
+
+   // eth
+   memcpy(buff + offset, &eth, sizeof(eth));
+   offset += sizeof(eth);
+ 
+   // ip
+   memcpy(buff + offset, &ip, sizeof(ip));
+   offset += sizeof(ip);
+
+   // udp
+   memcpy(buff + offset, &udp, sizeof(udp));
+   offset += sizeof(udp);
+
+   // udp data/payload
+   memcpy(buff + offset, msg, strlen(msg));
+   offset += strlen(msg);
+
+   print_hex(buff, offset, colors);
+
+   return offset;
 }
 
 
@@ -309,52 +413,60 @@ int main(int, char *[])
 
     // open
     if(tunTap.open("/dev/net/tun", "tuntap0", IFF_TAP | IFF_NO_PI) < 0)
-    {
+     {
         bye("tun open");
-    }
+     }
 
     // set ip addr
-    if(tunTap.set_ip_address(inet_addr("172.16.1.1"), 24) < 0)
-    {
+    if(tunTap.set_ip_address(inet_addr(localIP), 24) < 0)
+     {
         bye("tun set ip");
-    }
+     }
 
     // set hw addr
-    if(tunTap.set_hw_address(ether_aton("02:02:00:00:00:01")) < 0)
-    {
+    if(tunTap.set_hw_address(ether_aton(localHW)) < 0)
+     {
         bye("tun set ip");
-    }
+     }
 
     // set up, arp on
     if(tunTap.activate(true, true) < 0)
-    {
+     {
         bye("tun activate");
-    }
+     }
 
-    // set read blocking
-    if(tunTap.set_blocking(true) < 0)
-    {
+    // set non blocking
+    if(tunTap.set_blocking(false) < 0)
+     {
         bye("tun setblocking");
-    }
+     }
 
     while(bRunning)
-    {
-        char buff[2048] = {0};
+     {
+       char buff[2048] = {0};
 
-        const int num_read = tunTap.read(buff, sizeof(buff));
+       const int num_read = tunTap.read(buff, sizeof(buff));
 
-        if(num_read < 0)
-        {
-            bye("tun read");
-        }
-        else
-        {
-            const int num_write = bounce_frame(buff, num_read);
+       if(num_read > 0) 
+         {
+           const int num_write = parse_frame(buff, num_read);
 
-            if(num_write > 0)
-             {
-               tunTap.write(buff, num_write);
-             }
+           if(num_write > 0)
+            {
+              tunTap.write(buff, num_write);
+            }
+         }
+       else
+         {
+           // make soemthing up
+           const int num_write = build_udp(buff, sizeof(buff));
+
+          if(num_write > 0)
+           {
+             tunTap.write(buff, num_write);
+           }
+
+          sleep(1);
         }
     }
 
@@ -362,5 +474,3 @@ int main(int, char *[])
 
     return (0);
 }
-
-
